72f35c23af09b812791bfff89b015a913b711b74
hp <alyushabgp1996@gmail.com>
2020-08-05 13:51:32 +0530

    commit

--- large_2_4.py
+++ large_2_4.py
@@ -1,11 +1,3 @@
-print("enter numbers:")
-list1=[int(x) for x in input().split()]
-new_list = set(list1)
-new_list.remove(max(new_list))
-new_list1=new_list.copy()
-
-new_list1.remove(max(new_list1))
-
-new_list1.remove(max(new_list1))
-print("second largest number:", max(new_list))
-print("fourth largest number:", max(new_list1))
+list2=[45,69,71,52,56,885]
+list2.sort()
+print("second largest num:{0}, fourth largest num:{1}".format(list2[-2],list2[-4]))
